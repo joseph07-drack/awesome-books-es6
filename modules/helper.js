daf88f3caa719592376c052014f9b10311e95b09
jodrack <drack.sir01@gmail.com>
2022-04-25 15:53:44 +0200

    Add clearForm functionality and format to es6 the previous code

--- modules/helper.js
+++ modules/helper.js
@@ -1,17 +1,22 @@
 // remove the active class to all links
-function removeActiveClass() {
+const removeActiveClass = () => {
   links.forEach((link) => link.classList.remove('active'));
-}
+};
 
 // add nav-item-active class to the current link
-function selectCurrentActiveItem() {
+const selectCurrentActiveItem = () => {
   removeActiveClass();
   this.classList.add('active');
-}
+};
 
-function getDate() {
+const getDate = () => {
   document.querySelector('#date-time').innerText =
     luxon.DateTime.now().toLocaleString(
       luxon.DateTime.DATETIME_FULL_WITH_SECONDS
     );
-}
+};
+
+const clearForm = () => {
+  form.elements.title.value = '';
+  form.elements.author.value = '';
+};
