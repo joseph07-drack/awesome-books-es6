60bde87582e9c83341e928345a2eb4a5c172b5e6
jodrack <drack.sir01@gmail.com>
2022-04-25 15:47:43 +0200

    Add date using luxon package

--- modules/helper.js
+++ modules/helper.js
@@ -8,3 +8,10 @@ function selectCurrentActiveItem() {
   removeActiveClass();
   this.classList.add('active');
 }
+
+function getDate() {
+  document.querySelector('#date-time').innerText =
+    luxon.DateTime.now().toLocaleString(
+      luxon.DateTime.DATETIME_FULL_WITH_SECONDS
+    );
+}
