7ba9e078e42f2a3ffa1ec251069be17a13aacc85
jodrack <drack.sir01@gmail.com>
2022-04-25 16:08:54 +0200

    Initialize the main.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.form-input');
+
+const links = document.querySelectorAll('.nav-link');
+
+/* displaying the UI */
+const displayContent = new UI();
+const action = new Action();
+
+const saveInfo = document.querySelector('.form-container');
+const contactInfo = document.querySelector('.contact');
+const displayInfo = document.querySelector('.presentation');
