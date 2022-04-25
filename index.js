90730949371d471c4e3b76ee19a4911fb4d20ac4
jodrack <drack.sir01@gmail.com>
2022-04-25 16:22:12 +0200

    Add add book function using DOM

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ import Book from './modules/book';
 import UI from './modules/utils';
 import Helper from './modules/helper';
 
+// create instances for class
+const displayContent = new UI();
+const action = new Action();
+const helper = new Helper();
+
 const form = document.querySelector('.form-input');
 
 const links = document.querySelectorAll('.nav-link');
-
-/* displaying the UI */
-const displayContent = new UI();
-const action = new Action();
 
 const saveInfo = document.querySelector('.form-container');
 const contactInfo = document.querySelector('.contact');
 const displayInfo = document.querySelector('.presentation');
 
+// display books
 document.addEventListener('DOMContentLoaded', () => {
   displayContent.addingBooks();
   contactInfo.style.display = 'none';
   saveInfo.style.display = 'none';
+});
+
+// Adding a Book
+const addButton = document.querySelector('#add-btn');
+addButton.addEventListener('click', () => {
+  const title = form.elements.title.value;
+  const author = form.elements.author.value;
+  const newBook = new Book(title, author);
+
+  displayContent.saveBook(newBook);
+  action.saveBook(newBook);
+  helper.clearForm();
 });
